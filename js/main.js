9c25ce138d786ff557032371df21452a881fec44
Jason Quaccia <jlquaccia@gmail.com>
2016-10-21 18:48:43 -0700

    integrated angular and made date dynamic

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function() {
+  var app = angular.module('spotlightMidPage1', []);
+
+  app.controller('MainCtrl', ['$scope', function($scope) {
+    $scope.currentDate = new Date();
+  }]);
+})();
