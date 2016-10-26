1e4fa19a88a21c9cbc7bc993dc7e587c50e6a037
Jason Quaccia <jlquaccia@gmail.com>
2016-10-25 18:03:38 -0700

    finished assignment!

--- js/main.js
+++ js/main.js
@@ -4,5 +4,23 @@
   app.controller('MainCtrl', ['$scope', function($scope) {
     $scope.currentDate = new Date();
     $scope.toggle = false;
+    $scope.activePage = 1;
+    $scope.goTo = function(page) {
+      var $bookBlock = $('#bb-bookblock');
+
+      $bookBlock.bookblock('jump', page);
+    };
+
+    $scope.nextActive = function() {
+      if ($scope.activePage === 8) return;
+
+      $scope.activePage++;
+    };
+
+    $scope.prevActive = function() {
+      if ($scope.activePage === 1) return;
+
+      $scope.activePage--;
+    };
   }]);
 })();
